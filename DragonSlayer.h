24b91e3d9d9b6292fc7868f2e32d2d5e5457875a
DiaAudi <61022208+DiaAudi@users.noreply.github.com>
2021-09-11 02:49:34 +0000

    x

--- DragonSlayer.h
+++ DragonSlayer.h
@@ -5,11 +5,11 @@
 
 struct DragonSlayer : Character
 {
-	DragonSlayer(std::string name_, int hp_, int armor_);
-	const std::string& getName() override;
-	std::string getStats() override;
+    DragonSlayer(std::string name_, int hp_, int armor_);
+    const std::string& getName() override;
+    std::string getStats() override;
     void attack(Character& other) override;
 private:
     const std::string name;
-	std::unique_ptr<AttackItem> attackItem {new AttackItem() };
+    std::unique_ptr<AttackItem> attackItem {new AttackItem() };
 };
