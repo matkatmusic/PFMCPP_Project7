3cfd9375035a332797d04e88786177d816393ec3
DiaAudi <61022208+DiaAudi@users.noreply.github.com>
2021-09-11 02:44:10 +0000

    x

--- Dwarf.h
+++ Dwarf.h
@@ -4,9 +4,9 @@
 
 struct Dwarf : Character
 {
-	Dwarf(std::string name_, int hp_, int armor_);
-	const std::string& getName() override;
-	std::string getStats() override;
+    Dwarf(std::string name_, int hp_, int armor_);
+    const std::string& getName() override;
+    std::string getStats() override;
 private:
     const std::string name;
 };
